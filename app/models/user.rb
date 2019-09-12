1263086814c20023a14cdfe407a041abdd0908dd
long-lb0945 <ly.bao.long@sun-asterisk.com>
2019-09-12 11:55:38 +0700

    finish Chapter 9

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   VALID_EMAIL_REGEX = Settings.email_regex
 
   before_save{email.downcase!}
@@ -11,6 +13,35 @@ class User < ApplicationRecord
     uniqueness: {case_sensitive: false}
   validates :password, presence: true,
     length: {minimum: Settings.minimum_password_length}
+
+  class << self
+    def digest string
+      cost = if ActiveModel::SecurePassword.min_cost
+               BCrypt::Engine::MIN_COST
+             else
+               BCrypt::Engine.cost
+             end
+      BCrypt::Password.create string, cost: cost
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update remember_digest: User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false if remember_digest.blank?
+    BCrypt::Password.new remember_digest.is_password? remember_token
+  end
+
+  def forget
+    update remember_digest: nil
+  end
 
   has_secure_password
 end
