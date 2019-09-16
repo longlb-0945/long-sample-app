d5b950ee10188366084cda0d78f25b808c70db84
long-lb0945 <ly.bao.long@sun-asterisk.com>
2019-09-16 22:40:58 +0700

    finish Chapter_11

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,19 @@
-User.create!(name: "Example User",
-  email: "example@railstutorial.org",
+User.create!(name: "Ly Bao Long",
+  email: "kaimitamotoji@gmail.com",
   password: "11111111",
-  password_confirmation: "11111111")
+  password_confirmation: "11111111",
+  admin: true,
+  activated: true,
+  activated_at: Time.zone.now)
 
-99.times do |n|
+98.times do |n|
 name = Faker::Name.name
 email = "example-#{n+1}@railstutorial.org"
 password = "password"
 User.create!(name: name,
-  email: email,
+   email: email,
    password: password,
-   password_confirmation: password)
+   password_confirmation: password,
+   activated: true,
+   activated_at: Time.zone.now)
 end
